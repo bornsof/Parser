c9595c67a3fbae487e8a4f62ec57497d7d74c90f
bornsof <bornsof@mail.ru>
2013-10-02 21:19:05 +0400

    исправлена сигнатура окончания строки адреса

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ $content= file_get_contents('https://www.google.ru/search?q='.$qry);
 $f9=strripos($content,'<h3 class="r"><a href="');
 while ($f0!=$f9){
     $f0=strpos($content,'<h3 class="r"><a href="',$f1);
-    $f=$f0+23;
-    $f1=strpos($content,'"',$f);
+    $f=$f0+30; //смещение на размер поисковой строки.
+    $f1=strpos($content,'&amp;sa=',$f); //&amp;sa= - сигнатура окончания адреса в поиске
     $s[$i] =substr($content,$f,$f1-$f);
-    echo htmlspecialchars($s[$i]);
+    echo htmlspecialchars($s[$i]);  //странно но работает, переменная $i нигде сама не меняется))
     echo ('</br>');
     }
     
@@ -20,7 +20,7 @@ while ($f0!=$f9){
  
  
  
- echo ('$parser1:'.$parser1.'$parser2:'.$parser2);
+// echo ('$parser1:'.$parser1.'$parser2:'.$parser2);
  
-    echo htmlspecialchars($parser);
+   // echo htmlspecialchars($parser);
 ?>
